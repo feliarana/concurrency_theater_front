fdcf89cf6ef4e73296800b4327d1bc5c26a4e7ec
Felipe Arana <felipe.arana@petscreening.com>
2024-10-17 12:52:50 -0300

    add cable

--- src/hooks/useWebSocket.js
+++ src/hooks/useWebSocket.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const useWebSocket = (performanceId, setSeats) => {
   useEffect(() => {
     // Access the WebSocket URL from the environment variable
-    const wsUrl = import.meta.env.VITE_WEBSOCKET_URL || "ws://localhost:3000";
+    const wsUrl = import.meta.env.VITE_WEBSOCKET_URL || "ws://localhost:3000/cable";
 
     if (!wsUrl) {
       console.error("WebSocket URL not defined in environment variables.");
