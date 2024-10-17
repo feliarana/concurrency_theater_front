936357d63550c0c79f2a9c649c040a2b80b65ada
Felipe Arana <felipe.arana@petscreening.com>
2024-10-17 18:00:01 -0300

    Reset logic

--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from "react";
 import api from "../api/api";
+import useAuthStore from "../store/authStore";
 
 const Reset = () => {
+  const user = useAuthStore((state) => state.user);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
     const reset = async () => {
       try {
-        const response = await api.post("/tickets/reset");
+        const token = localStorage.getItem("token");
+        await api.post(`/tickets/reset`,
+          {},
+          {
+            headers: {
+              Authorization: `${token}`,
+            },
+          }
+        );
         setMessage("OK");
       } catch (error) {
         setMessage("Error resetting: " + error.message);
       }
     };
     reset();
-  }, []);
+  }, [user]);
 
   return (
     <div>
