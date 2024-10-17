0b0d6426b5a554e87e03d89fadf7d90cf9f84a5b
Felipe Arana <felipe.arana@petscreening.com>
2024-10-17 18:10:48 -0300

    Added logic to show no tickets if there are not any

--- src/components/MyTickets.jsx
+++ src/components/MyTickets.jsx
@@ -24,16 +24,20 @@ const MyTickets = () => {
   return (
     <div style={{ maxWidth: 400, margin: '0 auto' }}>
       <h1 style={{ textAlign: 'center' }}>Mis Tickets Comprados</h1>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        {myTickets.map((ticket) => (
-          <li key={ticket.id} style={{ backgroundColor: '#f9f9f9', padding: 10, margin: 10, border: '1px solid #ddd', borderRadius: 5 }}>
-            <h2 style={{ fontSize: 16, fontWeight: 'bold' }}>{ticket.performance.title}</h2>
-            <p style={{ fontSize: 14, color: '#666' }}>Fecha: {ticket.date} | Precio: {ticket.price}</p>
-            <p style={{ fontSize: 14, color: '#666' }}>Estado: {translateStatus(ticket.status)}</p>
-            <p style={{ fontSize: 14, color: '#666' }}>A nombre de: {ticket.user.name}</p>
-          </li>
-        ))}
-      </ul>
+      {myTickets.length === 0 ? (
+        <p>No tienes tickets comprados.</p>
+      ) : (
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          {myTickets.map((ticket) => (
+            <li key={ticket.id} style={{ backgroundColor: '#f9f9f9', padding: 10, margin: 10, border: '1px solid #ddd', borderRadius: 5 }}>
+              <h2 style={{ fontSize: 16, fontWeight: 'bold' }}>{ticket.performance.title}</h2>
+              <p style={{ fontSize: 14, color: '#666' }}>Fecha: {ticket.date} | Precio: {ticket.price}</p>
+              <p style={{ fontSize: 14, color: '#666' }}>Estado: {translateStatus(ticket.status)}</p>
+              <p style={{ fontSize: 14, color: '#666' }}>A nombre de: {ticket.user.name}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
